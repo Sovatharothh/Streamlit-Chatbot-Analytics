b354fd75783c415592809bbb651991173cb21deb
Sovatharothh <116065704+Sovatharothh@users.noreply.github.com>
2025-04-05 18:58:25 +0700

    fix models

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv
 from pandasai import SmartDataframe
-from google_gemini import GoogleGemini
+from pandasai.llm import GoogleGemini
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
